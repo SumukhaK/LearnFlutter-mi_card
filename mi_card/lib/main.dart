a5de769cc61ca87c42812e1f37aff8cb25075f86
sumukha <sumukh.aithal@gmail.com>
2021-02-19 12:00:11 +0530

    polishing up things

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -37,6 +38,59 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.0,
                     fontWeight: FontWeight.normal,
                     fontFamily: 'SourceSansPro'),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "+91-9164959595",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            letterSpacing: 1.5,
+                            //fontWeight: FontWeight.bold,
+                            fontFamily: 'SourceSansPro'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "sumukh.aithal@yahoomail.co.in",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            //fontWeight: FontWeight.bold,
+                            fontFamily: 'SourceSansPro'),
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           ),
